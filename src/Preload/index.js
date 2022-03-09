4f9b699c3ff6ad04c2bcc0bab792aad300bc9eea
RodAlexCoder <nasc.aleex@gmail.com>
2022-03-09 10:05:00 -0300

    Primeiro Commit inicio do App

---
+++ src/Preload/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Preload() {
+  return (
+    <View>
+        <Text>PreLoad Screen</Text>
+    </View>
+  )
+}
+
+export default Preload
---
+++ src/stacks/MainStack.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Preload from '../Preload'
+
+const Stack = createNativeStackNavigator() 
+
+
+export default function MainStack(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name='PreLoad' component={Preload}/>
+        </Stack.Navigator>
+    )
+}
