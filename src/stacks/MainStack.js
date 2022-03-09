13f44cfba38777bd03e53c365fd5226a213b9b86
RodAlexCoder <nasc.aleex@gmail.com>
2022-03-09 10:19:51 -0300

    Criando as Paginas SignIn e SignUp

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Preload from '../Preload'
+import Preload from '../screens/Preload'
 
 const Stack = createNativeStackNavigator() 
 
